0b868e9f9da29bba2e9bf9353af79c3a2b640b82
Gourab <gourabporey63@gmail.com>
2021-07-12 19:06:39 +0530

    enter

--- bitwiseOperators.cpp
+++ bitwiseOperators.cpp
@@ -1,1 +1,20 @@
-//
+//bitwise operators
+#include<iostream>
+using namespace std;
+
+int getBit(int n, int pos){
+    return ((n & (1 << pos))!=0);
+}
+int setBit(int n, int pos){
+    return (n | (1 << pos));
+}
+int clearBit(int n, int pos){
+    return (n & (~(1<<pos)));
+}
+int main(){
+    int a = 5;
+    cout << clearBit(a, 2);
+
+    return 0;
+}
+
